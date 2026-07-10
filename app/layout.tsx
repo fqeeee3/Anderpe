41f977b156d3f5dfc7c2bf9429286e3461bc0287
fqeeee3 <datandiboi@gmail.com>
2026-07-10 02:25:13 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,17 +22,7 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
-}
+       
 
 export const viewport: Viewport = {
   colorScheme: 'light dark',
