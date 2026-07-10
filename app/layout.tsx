720d28cd15471399b098b34bcc2610cdc44fcc8a
fqeeee3 <datandiboi@gmail.com>
2026-07-10 02:56:48 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,7 @@ export const metadata: Metadata = {
   title: 'Anderpe',
   description:
     'We build e-commerce businesses with cutting-edge technology and AI-driven strategies.',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-       
+}
 
 export const viewport: Viewport = {
   colorScheme: 'light dark',
